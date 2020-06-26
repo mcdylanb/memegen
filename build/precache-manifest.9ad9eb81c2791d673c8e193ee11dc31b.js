dae66644977ac16cd8a4bc96c2d2999459bd77bf
mcdylanb <dylansatgalab@gmail.com>
2020-06-26 10:16:54 +0800

    what happen

---
+++ build/precache-manifest.9ad9eb81c2791d673c8e193ee11dc31b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f39acc10457337ed88957240d01baa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe27e8765d1d523cbe4d",
+    "url": "/static/css/main.2033fa70.chunk.css"
+  },
+  {
+    "revision": "8dbee7c1f543895be480",
+    "url": "/static/js/2.7fa9911a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7fa9911a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe27e8765d1d523cbe4d",
+    "url": "/static/js/main.0428abd5.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  },
+  {
+    "revision": "6e5d6096c02e8c18ad95d40ae5bf0089",
+    "url": "/static/media/loading-png-gif.6e5d6096.gif"
+  }
+]);
